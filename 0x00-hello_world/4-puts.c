0188456758eb638c9a552c629d88845096b3e242
Adolf <kajeseadolf@gmail.com>
2023-06-15 08:03:31 -0700

    Hello, puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a line with puts function
+ * Return: o (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
