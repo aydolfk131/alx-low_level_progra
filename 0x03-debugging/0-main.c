ef7c4310f72dbddd6eb2836fa0faf993dc4d380a
Adolf <kajeseadolf@gmail.com>
2023-06-22 08:55:54 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*main - Testing functions positive or negative
+*Return: 0
+*/
+int main(void)
+{
+int i;
+I = 0;
+positive_or_negative
+return (0);
+}
+
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+/**
+*main - Entry point
+*Return: 0
+*/
+int main(void)
+{
+int i;
+printf("infinite loop incoming; (\n");
+i = 0
+/*while (I < 10)*/
+/* {*/
+/* putchar(i);*/
+/*}*/
+printf("infinite loop avoided! \\0/\n");
+return (0);
+}
+
