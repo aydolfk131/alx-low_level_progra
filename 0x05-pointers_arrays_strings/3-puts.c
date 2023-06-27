9f3dc73d27a95bf02b2dee555e08666d52ae1162
Adolf <kajeseadolf@gmail.com>
2023-06-27 05:53:06 -0700

    Strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdout.h>
+/**
+*_puts - a function that prints a string 
+*Return: Always 0 (Success)
+*/
+void _puts(char *str);
+{
+char *str;
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+_puts(str);
+return (0);
+}
+
