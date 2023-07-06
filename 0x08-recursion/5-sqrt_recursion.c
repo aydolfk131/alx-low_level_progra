92bda98eaced0864d39b8be80375ff7fa44250ee
Adolf <kajeseadolf@gmail.com>
2023-07-06 08:31:48 -0700

    5-sqrt_recursion.c task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,19 @@
 #include "main.h"
+int actual_root(int number, int root);
+/**
+ * actual_root - finds the root of a number
+ * @number: integer to evaluated
+ * @root: the result integer
+ * Return: the result
+ */
+int actual_root(int number, int root)
+{
+	if (root * root != number)
+		return (-1);
+	if (root * root == number)
+		return (root);
+	return (actual_root(number, root + 1));
+}
 /**
  * _sqrt_recursion - A function that returns the sqrt of a number
  * @n: integer
@@ -8,7 +23,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 1)
-		return (1);
-	return (_sqrt_recursion(n, 0));
+	return (actual_root(n, 0));
 }
