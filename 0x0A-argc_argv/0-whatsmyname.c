10090247b2ae5deb2cf9e2eaec32da244704ffb1
Adolf <kajeseadolf@gmail.com>
2023-07-10 01:50:23 -0700

    0-whatsmyname.c task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A function that prints its name
+ * @argc: the count
+ * @argv: the array of the count
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
